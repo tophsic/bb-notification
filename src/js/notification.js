5421718e5e8a62df885c8b6bdec838e5e51a57b7
Christophe Sicard <sicard.christophe@gmail.com>
2014-04-20 21:48:28 +0200

    Retab

--- src/js/notification.js
+++ src/js/notification.js
@@ -1,5 +1,5 @@
 /**
- *	@module notification/notification
+ *  @module notification/notification
  */
 var _ = require('underscore');
 var $ = require('jquery');
@@ -8,15 +8,15 @@ var Backbone = require('backbone');
 
 
 /**
- *	Bootstrapping de baobab: configuration d'underscore et backbone.
+ *  Bootstrapping de baobab: configuration d'underscore et backbone.
  */
 require('notification/bootstrap');
 
 
 
 var Notification = function(options) {
-	options || (options = {});
-	this.initialize.apply(this, arguments);
+  options || (options = {});
+  this.initialize.apply(this, arguments);
 };
 
 Notification.prototype.defaults = {
@@ -27,9 +27,9 @@ Notification.prototype.initialize = function(options) {
   this.options = options;
    _.defaults(this.options, this.defaults);
 
-	if (!this.options.url) {
-		throw new Error('Url must be set to use notifications');
-	}
+  if (!this.options.url) {
+    throw new Error('Url must be set to use notifications');
+  }
 };
 
 
