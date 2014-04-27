43fd34be7b5c9359c24440b92ab8f46efbb756e6
Christophe Sicard <sicard.christophe@gmail.com>
2014-04-27 09:26:49 +0200

    Notification view listen collection sync

--- spec/views/items.spec.js
+++ spec/views/items.spec.js
@@ -1,21 +1,18 @@
 var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
 
 describe("Items view", function () {
   before(function () {
     this.$fixture = $("<div id='notification-items'></div>");
+    this.View = require('notification/views/items');
   });
 
   beforeEach(function () {
     this.$fixture.empty().appendTo($("#fixtures"));
-
-    var View = require('notification/views/items');
-    this.view = new View({
-      el: this.$fixture
-    });
   });
 
   afterEach(function () {
-    this.view.remove();
   });
 
   after(function () {
@@ -23,12 +20,50 @@ describe("Items view", function () {
   });
 
   it("instanciate", function () {
+    var view = new this.View({
+      el: this.$fixture,
+      collection: Backbone.Events
+    });
+
     expect(this.view).to.be.an("object");
+
+    view.remove();
   });
 
-  it("can be rendered", function () {
+  it("instanciate wit an error", function () {
+    expect(function() {
+      var view = new this.View({
+        el: this.$fixture
+      });
+    }).to.throw(Error);
+  });
+
+  it("render", function () {
+    var view = new this.View({
+      el: this.$fixture,
+      collection: Backbone.Events
+    });
     var $items = $("#notification-items ul");
 
     expect($items.hasClass("notification-items")).to.be.true;
+
+    view.remove();
   });
+
+  it("listen collection sync", sinon.test(function () {
+    var collection = Backbone.Events;
+
+    var view = new this.View({
+      el: this.$fixture,
+      collection: collection
+    });
+
+    this.stub(view);
+
+    collection.trigger('sync');
+
+    expect(view.onCollectionSync).to.have.been.called;
+
+    view.remove();
+  }));
 });
--- src/js/notification/views/items.js
+++ src/js/notification/views/items.js
@@ -1,5 +1,5 @@
 /**
- *	@module notification/views/item
+ *  @module notification/views/item
  */
 var _ = require('underscore');
 var $ = require('jquery');
@@ -11,13 +11,28 @@ var ItemsView = Backbone.View.extend({
   template: _.template(require('notification/templates/items')),
 
   initialize: function(options) {
-		this.$el.html(this.template()) ;
+    this.options = options || {};
+
+    if (!this.options.collection) {
+      throw new Error('Collection must be set');
+    }
+
+    this.collection = this.options.collection;
+    this.listenTo(this.collection, {
+      "sync":  function() { this.onCollectionSync(); }
+    });
+
+    this.$el.html(this.template()) ;
     this.render();
   },
 
   render: function () {
     this.$el.html(this.template());
     return this;
+  },
+
+  onCollectionSync: function(collection, notifications) {
+    //TODO implement
   }
 });
 
