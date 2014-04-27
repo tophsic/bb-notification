9110e8e654d294f830ccd02c7b077b02ff08de4c
Christophe Sicard <sicard.christophe@gmail.com>
2014-04-27 19:50:24 +0200

    notification.views.notification dev

--- spec/views/notification.spec.js
+++ spec/views/notification.spec.js
@@ -1,6 +1,88 @@
-describe("Notification view", function () {
-  it("instanciate", function () {
-    var View = require('notification/views/notification');
-    expect(new View()).to.be.an("object");
+var $ = require('jquery');
+var Backbone = require('backbone');
+
+
+
+describe("Notification view", function() {
+
+
+
+  before(function() {
+    this.$fixture = $("<div id='notification'></div>");
+    this.View = require('notification/views/notification');
   });
+
+  beforeEach(function() {
+    this.$fixture.empty().appendTo($("#fixtures"));
+  });
+
+  after(function() {
+    $("#fixtures").empty();
+  });
+
+
+
+  describe("instantiate", function() {
+    it("normally", function() {
+      var view = new this.View({
+        el: this.$fixture,
+        collection: Backbone.Events
+      });
+      expect(view).to.be.an("object");
+    });
+
+    it("with an error", function() {
+      var View = this.View;
+      var fn = function() {
+        var view = new View();
+      };
+      expect(fn).to.throw(Error);
+    });
+  });
+
+  describe("render", function() {
+    beforeEach(function() {
+      this.view = new this.View({
+        el: this.$fixture,
+        collection: new Backbone.Collection()
+      });
+    });
+
+    afterEach(function() {
+      this.view.remove();
+    });
+
+    it("empty list", function() {
+      var $items = $("#notification .notification");
+
+      expect($items).to.have.length(1);
+      expect($items.hasClass("notify")).to.be.false;
+    });
+
+    it("after collection fetch", sinon.test(function() {
+      this.stub(this.view);
+
+      this.view.collection.trigger('sync', this.view.collection, [
+        {
+          link: "notification/1",
+          message: "Notification 1"
+        }
+      ]);
+
+      expect(this.view.render).to.have.been.called;
+    }));
+
+    it("list", function() {
+      this.view.collection.length = 1;
+      this.view.collection.trigger('sync', this.view.collection, []);
+
+      var $items = $("#notification .notification");
+
+      expect($items).to.have.length(1);
+      expect($items.hasClass("notify")).to.be.true;
+    });
+
+  });
+
+
 });
--- src/js/notification/views/notification.js
+++ src/js/notification/views/notification.js
@@ -8,7 +8,38 @@ var Backbone = require('backbone');
 
 
 var NotificationView = Backbone.View.extend({
+  defaults: {
+    template: 'notification/templates/notification',
+  },
 
+  initialize: function(options) {
+    this.options = options || {};
+    _.defaults(this.options, this.defaults);
+
+    if (!this.options.collection) {
+      throw new Error('Collection must be set');
+    }
+
+    if (!this.options.template) {
+      throw new Error('Template must be set');
+    }
+
+    this.collection = this.options.collection;
+    this.listenTo(this.collection, {
+      "sync":  function() { this.render(); }
+    });
+
+    this.template = _.template(require(this.options.template));
+
+    this.render();
+  },
+
+  render: function() {
+    this.$el.html(this.template({
+      collection: this.collection
+    }));
+    return this;
+  }
 });
 
 
