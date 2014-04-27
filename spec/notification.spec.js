c7d7a28deef772d30580bf750088d61a07f3783b
Christophe Sicard <sicard.christophe@gmail.com>
2014-04-27 22:15:00 +0200

    Notification app and spec

--- spec/notification.spec.js
+++ spec/notification.spec.js
@@ -1,12 +1,27 @@
 describe("Notification", function() {
+
+
+
   before(function() {
+    this.$fixture = $("<div id='notification'></div>");
     this.Notification = require('notification');
   });
+
+  beforeEach(function() {
+    this.$fixture.empty().appendTo($("#fixtures"));
+  });
+
+  after(function() {
+    $("#fixtures").empty();
+  });
+
+
 
   describe("instantiate", function() {
     it("normally", function() {
       var notification = new this.Notification({
-          url: 'notification'
+          notification: { el: "#notification" },
+          url: "notification"
       });
       expect(notification).to.be.an("object");
     });
@@ -19,4 +34,25 @@ describe("Notification", function() {
       expect(fn).to.throw(Error);
     });
   });
+
+  it("listen", sinon.test(function() {
+      var clock = sinon.useFakeTimers(),
+          notification = new this.Notification({
+            notification: { el: "#notification" },
+            url: 'notification'
+          })
+          ;
+
+      this.spy(notification.collection, 'fetch');
+
+      notification.listen();
+
+      expect(notification.collection.fetch.callCount).to.be.equal(1);
+
+      clock.tick(10001);
+      expect(notification.collection.fetch.callCount).to.be.equal(2);
+
+      clock.tick(50001);
+      expect(notification.collection.fetch.callCount).to.be.equal(7);
+  }));
 });
--- src/js/notification.js
+++ src/js/notification.js
@@ -15,22 +15,76 @@ require('notification/bootstrap');
 
 
 var Notification = function(options) {
-  options || (options = {});
   this.initialize.apply(this, arguments);
 };
 
 Notification.prototype.defaults = {
-  url: null
+  url: null,
+  timeout: 10000,
+  collection: 'notification/collections/notification',
+  model: 'notification/models/notification',
+  notification: {
+    el: null,
+    view: 'notification/views/notification',
+    template: 'notification/templates/notification'
+  },
+  items: {
+    el: null,
+    view: 'notification/views/items',
+    template: 'notification/template/items',
+    item: {
+      view: 'notification/views/item',
+      template: 'notification/template/item',
+    }
+  }
 };
 
 Notification.prototype.initialize = function(options) {
-  this.options = options || {};
-   _.defaults(this.options, this.defaults);
+  this.options = _.clone(this.defaults);
+  this.options = _.merge(this.options, options);
 
   if (!this.options.url) {
     throw new Error('Url must be set to use notifications');
   }
+
+  if (!this.options.notification.el) {
+    throw new Error('El must be set to use notifications');
+  }
+
+  var Collection        = require(this.options.collection),
+      NotificationView  = require(this.options.notification.view),
+      ItemsView         = require(this.options.items.view)
+      ;
+
+  this.collection = new Collection([
+      require(this.options.model)
+  ], {
+    url: this.options.url
+  });
+
+  this.notification = new NotificationView({
+    el: this.options.notification.el,
+    template: this.options.notification.template,
+    collection: this.collection
+  });
+
+  this.items = new ItemsView({
+    el: this.options.items.el,
+    template: this.options.items.template,
+    collection: this.collection,
+    items: this.options.items.item
+  });
 };
+
+Notification.prototype.listen = function() {
+  var app = this;
+
+  this.collection.fetch();
+
+  setTimeout(function() {
+    app.listen();
+  }, app.options.timeout);
+}
 
 
 
